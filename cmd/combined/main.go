75b22ed53e42f0032756b85263387298e97f7458
Eric Daniels <eric@erdaniels.com>
2021-01-07 16:45:25 -0500

    remove fix for https://github.com/golang/go/issues/34053

--- cmd/combined/main.go
+++ cmd/combined/main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"flag"
-
 	"github.com/edaniels/golinters/deferfor"
 	"github.com/edaniels/golinters/errresp"
 	"github.com/edaniels/golinters/mustcheck"
@@ -11,16 +9,6 @@ import (
 
 	"golang.org/x/tools/go/analysis/unitchecker"
 )
-
-func init() {
-	// go vet always adds this flag for certain packages in the standard library,
-	// which causes "flag provided but not defined" errors when running with
-	// custom vet tools.
-	// So we just declare it here and swallow the flag.
-	// See https://github.com/golang/go/issues/34053 for details.
-	// TODO: Remove this once above issue is resolved.
-	flag.String("unsafeptr", "", "")
-}
 
 func main() {
 	unitchecker.Main(
